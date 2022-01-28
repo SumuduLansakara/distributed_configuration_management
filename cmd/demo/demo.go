fb8987840133f52a874c739b4e93779807cb6088
Sumudu Lansakara <sumudu.lansakara@ppro.com>
2022-01-28 23:07:40 +0100

    Add weather station demo

--- cmd/demo/demo.go
+++ cmd/demo/demo.go
@@ -1,77 +1,23 @@
 package main
 
 import (
-	"go.uber.org/zap"
-	"go_client/pkg/communicator"
-	"go_client/pkg/component"
+	"go_client/internal/demo"
 	"go_client/pkg/utils"
 	"time"
 )
 
-func checkErrors(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
 	utils.InitLogging()
-	communicator.InitClient()
-	defer communicator.DestroyClient()
 
-	// connect two components
-	comp1, err := component.NewLocalComponent("thekind", "thename1")
-	checkErrors(err)
-	comp1.Connect()
-	zap.L().Info("components list", zap.Any("components", comp1.ListComponents("")))
+	system := demo.System{}
+	system.Start()
 
-	// comp 1 watch comp 2 parameter events
-	var remoteComp2 *component.RemoteComponent
-	comp1.WatchComponents(
-		comp1.Kind,
-		func(stub *component.RemoteComponentStub) {
-			remoteComp2 = stub.Spawn()
-			comp1.WatchParameters(
-				stub.Id,
-				func(key, val string, isModified bool) {
-					zap.L().Info("param set", zap.String("key", key), zap.String("val", val), zap.Bool("IsMod", isModified))
-				},
-				func(key string) {
-					zap.L().Info("param delete", zap.String("key", key))
-				},
-			)
-		},
-		func(stub *component.RemoteComponentStub) {
-			zap.L().Info("component deleted", zap.Any("comp", stub))
-		},
-	)
+	go system.StartTemperatureSensor()
+	go system.StartAC()
+	go system.StartDisplay()
 
-	comp2, err := component.NewLocalComponent("thekind", "thename2")
-	checkErrors(err)
-	comp2.Connect()
+	system.InitAcController()
 
-	// add new comp2 parameter
-	comp2.SetParam("foo", "bar")
-	for !comp2.IsParamSet("foo") {
-	}
-	// re-read before remote change
-	println(">>>")
-	println(comp2.GetParam("foo"))
-	for remoteComp2 == nil {
-	}
-	println(remoteComp2.GetParam("foo"))
-	// change via remote
-	println("===")
-	remoteComp2.SetParam("foo", "baz")
-	time.Sleep(20 * time.Microsecond)
-	// re-read after remote change
-	println(comp2.GetParam("foo"))
-	println(remoteComp2.GetParam("foo"))
-	println("<<<")
-
-	comp2.DeleteParam("foo")
-
-	// disconnect
-	comp2.Disconnect()
-	comp1.Disconnect()
+	time.Sleep(time.Second * 1000)
+	system.Stop()
 }
