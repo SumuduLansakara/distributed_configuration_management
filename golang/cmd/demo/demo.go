903143402dbc11597121d478ac7bee496f0350fb
Sumudu Lansakara <sumudu.lansakara@gmail.com>
2022-02-15 09:27:32 +0100

    Start single component based on command line args

--- golang/cmd/demo/demo.go
+++ golang/cmd/demo/demo.go
@@ -3,15 +3,41 @@ package main
 import (
 	"go_client/internal/prototype"
 	"go_client/pkg/utils"
+	"os"
 	"time"
+
+	"go.uber.org/zap"
 )
 
 func main() {
+	time.Sleep(2 * time.Second) // FIXME: delay till etcd cluster starts
+
 	utils.InitLogging()
+	logger := zap.L()
+	logger.Debug("component started", zap.Any("args", os.Args))
+
+	if len(os.Args) != 2 {
+		zap.L().Panic("Invalid argument count")
+	}
 
 	system := prototype.System{}
 	system.Init()
-	system.Start()
+
+	switch os.Args[1] {
+	case "temperature-sensor":
+		system.StartTemperatureSensor()
+	case "humidity-sensor":
+		system.StartHumiditySensor()
+	case "display":
+		system.StartDisplayUnit()
+	case "airconditioner":
+		system.StartAC()
+	case "airconditioner-controller":
+		system.StartACController()
+	default:
+		zap.L().Panic("Unsupported component name", zap.String("name", os.Args[1]))
+	}
+	zap.L().Debug("started", zap.String("name", os.Args[1]))
 
 	// stop the simulation after 1 minute
 	time.Sleep(time.Second * 60)
--- golang/internal/prototype/system.go
+++ golang/internal/prototype/system.go
@@ -2,43 +2,59 @@ package prototype
 
 import (
 	"go_client/pkg/communicator"
+	"time"
 )
 
+type ComponentI interface {
+	Start()
+	Disconnect()
+}
+
 type System struct {
-	temperatureSensor *TemperatureSensor
-	humiditySensor    *HumiditySensor
-	displayUnit       *DisplayUnit
-	acUnit            *AirConditioner
-	acController      *AirConditionerController
+	components []ComponentI
 }
 
 func (s *System) Init() {
+	time.Sleep(2 * time.Second)
 	communicator.InitClient()
-	// create sensors
-	s.temperatureSensor = CreateTemperatureSensor("ts-1")
-	s.humiditySensor = CreateHumiditySensor("hs-1")
-	// create display-unit
-	s.displayUnit = CreateDisplayUnit("disp-1")
-	// create air-conditioner controller
-	s.acController = CreateAirConditionerController("ac-ctl-1")
-	// create air-conditioner
-	s.acUnit = CreateAirConditioner("ac-1")
-	s.acUnit.SetParam("active", "false")
 }
 
-func (s *System) Start() {
-	go s.temperatureSensor.Start()
-	go s.humiditySensor.Start()
-	go s.displayUnit.Start()
-	go s.acUnit.Start()
-	s.acController.Start()
+func (s *System) StartTemperatureSensor() {
+	ts := CreateTemperatureSensor("ts-1")
+	s.components = append(s.components, ts)
+	go ts.Start()
+}
+
+func (s *System) StartHumiditySensor() {
+	hs := CreateHumiditySensor("hs-1")
+	s.components = append(s.components, hs)
+	go hs.Start()
+}
+
+func (s *System) StartDisplayUnit() {
+	disp := CreateDisplayUnit("disp-1")
+	s.components = append(s.components, disp)
+	time.Sleep(1 * time.Second) // FIXME: delay till sensors connect
+	go disp.Start()
+}
+
+func (s *System) StartAC() {
+	ac := CreateAirConditioner("ac-1")
+	s.components = append(s.components, ac)
+	ac.SetParam("active", "false")
+	go ac.Start()
+}
+
+func (s *System) StartACController() {
+	acCtl := CreateAirConditionerController("ac-ctl-1")
+	s.components = append(s.components, acCtl)
+	time.Sleep(1 * time.Second) // FIXME: delay till AC connect
+	acCtl.Start()
 }
 
 func (s *System) Stop() {
-	s.acController.Disconnect()
-	s.acUnit.Disconnect()
-	s.displayUnit.Disconnect()
-	s.humiditySensor.Disconnect()
-	s.temperatureSensor.Disconnect()
+	for _, comp := range s.components {
+		comp.Disconnect()
+	}
 	communicator.DestroyClient()
 }
